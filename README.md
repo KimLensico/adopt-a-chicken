dfac76f22ea73f3982d456a4205d294e5c410c2e
KIMBERLY LENSICO <KIMBERLYLENSICO@OUTLOOK.COM>
2020-08-20 02:58:21 -0700

    added 3 java

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # adopt-a-chicken
 adopt a chicken, they have different personalities
+
+BG color change code:
+https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_style_backgroundcolor 
+
+scroll to top
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
+
---
+++ lit-with-chicks.js
@@ -0,0 +1,9 @@
+'use strict'
+
+if (confirm('Please confirm that you are over the age of eighteen to legally adopt a chicken.')) {
+    document.write('<section>' + 'Welcome to the Nest!' + '</section>')
+} else {
+    document.write('<section>' + 'A chick cannot adopt a chick!' + '</section>')
+} 
+
+object.style.backgroundColor = "color|transparent|initial|inherit"
