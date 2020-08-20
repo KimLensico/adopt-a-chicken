a3da44071105ed256ff562c9b9f95bd4da0d7aa0
KIMBERLY LENSICO <KIMBERLYLENSICO@OUTLOOK.COM>
2020-08-20 03:13:37 -0700

    Added input

--- lit-with-chicks.js
+++ lit-with-chicks.js
@@ -1,5 +1,15 @@
 'use strict'
 
+var question = prompt('Are chickens friends? (Yes/Not Yes)');
+
+alert(Answer: ABSOLUTELY);
+
+if (question === 'Yes'){
+alert('<3')
+ else {
+alert('YES THEY ARE!!')
+ }
+  
 if (confirm('Please confirm that you are over the age of eighteen to legally adopt a chicken.')) {
     document.write('<section>' + 'Welcome to the Nest!' + '</section>')
 } else {
